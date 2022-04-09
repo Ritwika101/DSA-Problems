6e24936d5f795b9814ee92ee3f16c846671d274c
Ritwika Pal <60825125+Ritwika101@users.noreply.github.com>
2022-04-09 16:37:00 +0530

    Create Permutations.cpp

---
+++ DP/Permutations.cpp
@@ -0,0 +1,19 @@
+void solve(vector<int> nums, vector<vector<int>>&ans, int i){
+        if(i>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }   
+        
+        for(int k=i;k<nums.size();k++){
+            swap(nums[i],nums[k]);
+            solve(nums,ans,i+1);
+            swap(nums[i],nums[k]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        int i=0;
+        solve(nums, ans, i);
+        return ans;
+    }
+
