d3db5fd1a8cdcd08ecd20dd7ffb59680160ffee8
Ritwika Pal <60825125+Ritwika101@users.noreply.github.com>
2022-04-12 10:10:33 +0530

    Update Count Of Substrings with Atmost K Unique Characters.cpp

--- Hashmap/Count Of Substrings with Atmost K Unique Characters.cpp
+++ Hashmap/Count Of Substrings with Atmost K Unique Characters.cpp
@@ -22,11 +22,10 @@ int main(){
     ll distinct=0;
     ll ans=0;
     for(int acq=0;acq<n;acq++){
-        //cout<<rel<<"*"<<distinct<<endl;
         if(m[s[acq]]==0)
             distinct+=1;
         m[s[acq]]+=1;
-        while(rel<=acq && distinct>k){
+        while(rel<acq && distinct>k){
             m[s[rel]]-=1;
             if(m[s[rel]]==0)
                 distinct-=1;
