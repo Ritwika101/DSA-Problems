8fe735d1e0bcf0c67ca329985c7298fb396b7cff
Ritwika Pal <60825125+Ritwika101@users.noreply.github.com>
2022-04-15 15:15:38 +0530

    Create Count pairs with given sum - GFG.cpp

---
+++ Hashmap/Count pairs with given sum - GFG.cpp
@@ -0,0 +1,16 @@
+int getPairsCount(int nums[], int n, int target) {
+        // code here
+        set<pair<int,int>>s;
+        int ans=0;
+        unordered_map<int,int>m;
+        for(int i=0;i<n;i++){
+            int a = nums[i];
+            int b = target - a;
+            if(m.find(b)!=m.end()){
+                ans+=m[b];
+            }
+            m[a]+=1;
+        }
+        return ans;
+        
+    }
