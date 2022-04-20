a67aa7e7b4f5e72407bac9f335db0dd155c2eb86
Ritwika Pal <60825125+Ritwika101@users.noreply.github.com>
2022-04-20 23:47:15 +0530

    Create 11. Container With Most Water.cpp

---
+++ Two pointers/11. Container With Most Water.cpp
@@ -0,0 +1,12 @@
+int maxArea(vector<int>& height) {
+        int i=0, j=height.size()-1;
+        int area=0;
+        while(i<j){
+            area=max(area, (min(height[i],height[j])*(j-i)) );
+            if(height[i]<height[j])
+                i++;
+            else
+                j--;
+        }
+        return area;
+    }
