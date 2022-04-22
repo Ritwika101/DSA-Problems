8a2e49836f21e7ae591dce757cc0c13e59aa380e
Ritwika Pal <60825125+Ritwika101@users.noreply.github.com>
2022-04-22 22:15:13 +0530

    Create 101. Symmetric Tree.cpp

---
+++ Trees/101. Symmetric Tree.cpp
@@ -0,0 +1,15 @@
+  bool solve(TreeNode* p,TreeNode* q){
+        if(!p && !q)
+            return true;
+        if(!p || !q)
+            return false;
+        if(p->val != q->val)
+            return false;
+        return (solve(p->left,q->right) && solve(p->right,q->left));
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root)
+            return true;
+       
+        return solve(root->left,root->right);
+    }
