d371b36024130bb053c3f79c5df04bd2c8d4726b
Ritwika Pal <60825125+Ritwika101@users.noreply.github.com>
2022-05-05 23:41:28 +0530

    Create 1791. Find Center of Star Graph.cpp

---
+++ Graph/1791. Find Center of Star Graph.cpp
@@ -0,0 +1,14 @@
+int findCenter(vector<vector<int>>& edges) {
+        int n = edges.size()+1;
+        vector<int>degree(n+1,0);
+      
+        for(int i=0;i<edges.size();i++){
+            degree[edges[i][0]]++;
+            degree[edges[i][1]]++;
+        }
+        for(int i=1;i<n+1;i++){
+            if(degree[i]==n-1)
+                return i;
+        }
+        return -1;
+    }
