227c7d9a0928528ff14ed977c131a7e8af3fc539
Ritwika Pal <60825125+Ritwika101@users.noreply.github.com>
2022-06-10 05:10:02 +0530

    Create 219. Contains Duplicate II.cpp

---
+++ Hashmap/219. Contains Duplicate II.cpp
@@ -0,0 +1,14 @@
+bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int>m;
+        for(int i=0;i<n;i++){
+            if(m.find(nums[i])!=m.end()){
+                if(abs(m[nums[i]]-i)<=k){
+                     return true;
+                }
+                   
+            }
+            m[nums[i]]=i;
+        }
+        return false;
+    }
