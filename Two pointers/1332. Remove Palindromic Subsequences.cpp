58032fa5b935b39e8f10222fd74a85f805966f1a
Ritwika Pal <60825125+Ritwika101@users.noreply.github.com>
2022-06-10 04:45:19 +0530

    Create 1332. Remove Palindromic Subsequences.cpp

---
+++ Two pointers/1332. Remove Palindromic Subsequences.cpp
@@ -0,0 +1,11 @@
+
+int removePalindromeSub(string s) {
+        int n = s.size();
+        int i=0, j=n-1;
+        while(i<j){
+            if(s[i]!=s[j]) return 2;
+            i++;
+            j--;
+        }
+        return 1;
+    }
