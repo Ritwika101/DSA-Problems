a9b9f5cd4e11e6063c83a1c21b290332f06cd51e
Ritwika Pal <60825125+Ritwika101@users.noreply.github.com>
2022-06-10 04:53:17 +0530

    Create 704. Binary Search.cpp

---
+++ Binary Search/704. Binary Search.cpp
@@ -0,0 +1,15 @@
+int search(vector<int>& nums, int target) {
+        int s=0;
+        int e=nums.size()-1;
+        sort(nums.begin(),nums.end());
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target)
+                s=mid+1;
+            else
+                e=mid-1;
+        }
+        return -1;
+    }
